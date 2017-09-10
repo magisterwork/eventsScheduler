04414bff048b62dd6576d9173fdc4866c3a9c0fa
Andrey <andreykudr@yahoo.com>
2017-09-10 22:39:57 +0300

    springboot init

---
+++ src/main/java/org/spree/vkscheduler/VkschedulerApplication.java
@@ -0,0 +1,12 @@
+package org.spree.vkscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkschedulerApplication.class, args);
+	}
+}
